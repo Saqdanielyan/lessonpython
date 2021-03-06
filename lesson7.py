cbbb92a13c4d4528fe5a43a0f8133cdd778a3049
Saqdanielyan <80157968+Saqdanielyan@users.noreply.github.com>
2021-03-06 17:07:43 +0400

    lesson7

---
+++ lesson7.py
@@ -0,0 +1,67 @@
+# color = input('color ? ').upper() == 'GOLD'
+# phone = int(input('iphone ? ')) == 12
+# uneq = input('arka e modely? ').upper()=='YES'
+# res = color and phone and uneq
+# print(res)
+
+
+
+##2) `
+# balance = 0
+# quesition1 = input('erb e texi unecel avarayri chakatamrty a)127\n b)451\n c)487\n d)504\n ').upper() == 'B' 
+# balance+=500
+# print(balance,quesition1)
+# quesition2 = input('erb e stexcvel pythony? a)1991\n b)2001\n c)1865\n d)2001 ').upper() == 'A'
+# balance+=500
+# print(balance,quesition2)
+# quesition3 = input('inchi e havasar 5i qarakusin a)1\n b)25\n c)48\n d)54 ').upper() == 'B' 
+# balance+=500
+# print(balance,quesition3)
+# quesition4 = input('ov e qo pythoni usucichy? a)Arno\n b)vzgo\n c)chut\n d)grno ').upper() == 'A' 
+# balance+=500
+# print(balance,quesition4)
+# quesition5 = input('ory qani jam uni a)24\n b)74\n c)65\n d)32 ').upper() == 'A'
+# balance+=500
+# print(balance,quesition5)
+
+
+##3)
+# year =int(input('year ?'))
+# res = year >= 17 and year <= 25
+# print(res)
+
+
+##4)
+# car = ('bmw audi mclaren')
+# car1 = input('inch meqena eq uzum? ')
+# year = ('2017 2018 2020')
+# year1 = input('vor tvakani? ')
+# res = car1 in car
+# res1 = year1 in year
+# x = res and res1
+# print(x)
+
+# ##5)
+# res = a**3 + b**3 + 3ab(a+b)
+# print(res)
+
+# import random
+
+# number = ('2, 4, 6, 8, 5')
+# pc = random.randint(0,10)
+# res = str(pc) in number
+# print(res)
+
+# import math
+
+# number = int(input('number? ')) ##tvi factorial
+# x = math.factorial(number)
+# print(x)
+
+
+# gram = int(input('gram? '))
+# sigaret = gram // 20
+# mnacord = gram % 20
+# print(sigaret,mnacord)
+# tup = sigaret // 20
+# print(tup)
