8a5211af9910a145b6eb00e6ab0462eeac2649fb
Saqdanielyan <80157968+Saqdanielyan@users.noreply.github.com>
2021-03-06 16:47:00 +0400

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,30 @@
+# print("Hello world")
+# print('anun')
+
+# name = 'Ani'
+# print(name)
+
+# name = input('What is your name?')
+# age = input('How old are you?')
+# phone = input('phone:') 
+# email = input('Please mention you email addres')
+
+# print('Hello', name,'age', age, "phone", phone, 'email', email)
+
+# born = int(input('Born:'))
+# print(2021 - born)
+
+# A = int(input('erkarutyun '))
+# B = int(input('laynutyun '))
+# s = A*B
+# print(s)
+
+
+# print('Tell me something boy\n\tare you happy in this modern world\nor do you need more')
+
+
+# a = int(input('number '))
+# h = int(input('h '))
+# s = 0.5*a*h
+# print(s)
+
